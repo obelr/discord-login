40a1d2908edfc310010f6c2d97edb13d372aac4a
obel <rdebebe@kifiya.et>
2024-01-03 12:35:16 +0300

    add icon and avatar placeholder

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { useState, useEffect } from "react";
 import { getDiscordUser } from "../api/discord";
+import Image from 'next/image';
+
 const Profile = () => {
   const [loading, setLoading] = useState(false);
   const [user, setUser] = useState<any>(null);
@@ -23,23 +25,39 @@ const Profile = () => {
   }, []);
 
   return (
-    <div>
-      {" "}
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="text-center">
-          {user && (
-            <div>
-              <p className="bg-red-500 text-white p-2 mb-2">
-                User ID: {user.id}
-              </p>
-              <p className="bg-red-500 text-white p-2">
-                Username: {user.username}
-              </p>
-            </div>
-          )}
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="mb-8">
+        <div
+        className=" mb-8 flex items-center justify-center">
+     <span>
+              {user.avatar ? (
+                <Image
+                  src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
+                  alt="Avatar"
+                  width={64}
+                  height={64}
+                  className="rounded-full"
+                />
+              ) : (
+                <Image
+                  src="/avatar.png" // Replace with your placeholder image
+                  alt="Placeholder Avatar"
+                  width={64}
+                  height={64}
+                  className="rounded-full"
+                />
+              )}
+            </span>
         </div>
+        {user && (
+          <div>
+       
+            <p className="bg-red-500 text-white p-2 mb-2">User ID: {user.id}</p>
+            <p className="bg-red-500 text-white p-2">Username: {user.username}</p>
+          </div>
+        )}
       </div>
     </div>
   );
-};
+              }  
 export default Profile;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 // src/app/page.tsx
-"use client"
-import { useState, useEffect } from 'react';
-import { getDiscordLoginURL, getDiscordUser } from './api/discord';
+"use client";
+import { useState, useEffect } from "react";
+import { getDiscordLoginURL, getDiscordUser } from "./api/discord";
+import { FaDiscord } from "react-icons/fa";
 
 const Home: React.FC = () => {
   const [user, setUser] = useState<any>(null);
@@ -12,24 +13,20 @@ const Home: React.FC = () => {
     window.location.href = getDiscordLoginURL();
   };
 
-  
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="text-center">
-       
-          <h1 className="text-4xl font-bold mb-4">Next.js Discord Login</h1>
-      
-          <button
-            onClick={handleDiscordLogin}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Login with Discord
-          </button>
-   
+    <div className="text-center">
+      <h1 className="text-4xl font-bold mb-8"> Discord Login</h1>
 
-       
-      </div>
+      <button
+        onClick={handleDiscordLogin}
+        className="bg-blue-500 flex items-center justify-center hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        <FaDiscord size={50} className="mr-4" />
+        <span>Login with Discord</span>
+      </button>
     </div>
+  </div>
   );
 };
 
